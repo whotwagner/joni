fc85e1eb76cebf7a174420e440188997ecd03ca8
Wolfgang Hotwagner <code@feedyourhead.at>
2020-12-08 21:23:24 +0100

    added logger

--- bin/jon.py
+++ bin/jon.py
@@ -3,22 +3,31 @@
 import sys
 from joni import joni
 import argparse
+import logging
+
+loglevel = logging.INFO
 
 parser = argparse.ArgumentParser(description='Joni Joni, Yes Papa...')
+parser.add_argument('-c','--cfgfile',type=str, help="Set config file",default="docs/config.yml")
+parser.add_argument('-L','--logfile',type=str, help="Set logfile")
+parser.add_argument('-d','--debug',action='store_true', help="Enable debugging(verbose logfile)")
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('--list', help="List music-database",action='store_true')
 group.add_argument('--start',type=str, help="Runs a playlist")
 group.add_argument('--stop', help="Stops the player",action='store_true')
 args = parser.parse_args()
 
-print(vars(args))
-
 if args.start and args.stop:
     print("Error: use only one of '--start ID' or '--stop'")
     sys.exit(1)
 
+if args.debug:
+    loglevel = logging.DEBUG
 
-yespapa = joni.Joni("docs/config.yml")
+if args.logfile:
+    logging.basicConfig(filename=args.logfile,  level=loglevel)
+
+yespapa = joni.Joni(args.cfgfile)
 
 if args.start:
     yespapa.start(args.start)
@@ -31,4 +40,3 @@ else:
     sys.exit(1)
 
 sys.exit(0)
-
--- joni/joni.py
+++ joni/joni.py
@@ -1,4 +1,5 @@
 import yaml
+import logging
 from pathlib import Path
 from .model import ConfigModel
 from mpd import MPDClient
@@ -9,14 +10,14 @@ class Joni:
     client = None
 
     def __init__(self, cfgfile='test.yml'):
-        print("Config-File: %s" % cfgfile)
+        logging.debug("Config-File: %s", cfgfile)
         self.cfgfile = cfgfile
         self.readCfg(self.cfgfile)
         self.client = MPDClient()
         self.client.timeout = self.config.timeout
         self.client.connect(self.config.mpdhost, self.config.mpdport)
         self.client.setvol(self.config.volume)
-        print("mpd_version: %s" % self.client.mpd_version)
+        logging.debug("mpd_version: %s", self.client.mpd_version)
 
     def readCfg(self,cfgfile):
         path = Path(cfgfile)
@@ -24,15 +25,15 @@ class Joni:
             ymldata = yaml.load(f, Loader=yaml.FullLoader)
         try:
             self.config = ConfigModel(**ymldata)
-            print(self.config.tracks)
         except ValidationError as e:
             print(e.errors())
+            logging.error(e.errors())
         f.close()
 
     def find(self,uid):
-        print("Searching for \"%s\"" % uid)
+        logging.info("Searching for \"%s\"", uid)
         for track in self.config.tracks:
-            print("testing \"%s\" against \"%s\"" % (uid,track.id))
+            logging.debug("testing \"%s\" against \"%s\"",uid,track.id)
             if str(track.id) == str(uid):
                 return track
         return None
@@ -49,19 +50,19 @@ class Joni:
     def start(self,uid):
         track = self.find(uid)
         if track != None:
-            print("Found: %s" % track.play)
             if track.volume:
-                print("Set volume to: %s " % track.volume)
+                logging.info("Set volume to: %s ", track.volume)
                 self.client.setvol(track.volume)
             else:
                 self.client.setvol(self.config.volume)
             self.client.clear()
             for item in track.play:
                 self.client.add(item)
+            logging.info("Start playing...")
             self.client.play()
         else:
-            print("NEW NFC-CARD FOUND")
+            logging.info("NEW NFC-CARD FOUND: %s", uid)
 
     def stop(self):
-        print("Stopping..")   
+        logging.info("Stopping...")
         self.client.stop()
